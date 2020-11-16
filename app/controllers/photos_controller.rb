404293bcc1f04fc15ef6ea1be205421190866120
NattuSakurai <71743081+NattuSakurai@users.noreply.github.com>
2020-11-16 12:26:13 +0900

    詳細画面の実装

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,5 @@
 class PhotosController < ApplicationController
+  before_action :set_photo, only: [:show, :update, :edit, :destroy]
 
   def index
     @photos = Photo.order("created_at DESC")
@@ -18,11 +19,9 @@ class PhotosController < ApplicationController
   end
 
   def edit
-    @photo = Photo.find(params[:id])
   end
 
   def update
-    @photo = Photo.find(params[:id])
     if @photo.update(photo_params)
       redirect_to root_path
     else
@@ -31,12 +30,20 @@ class PhotosController < ApplicationController
   end
 
   def destroy
-    @photo = Photo.find(params[:id])
     @photo.destroy
     redirect_to request.referer
   end
+
+  def show
+  end
+
+
   private
   def photo_params
     params.require(:photo).permit(:title, :image, :description, :place_id)
+  end
+
+  def set_photo
+    @photo = Photo.find(params[:id])
   end
 end
